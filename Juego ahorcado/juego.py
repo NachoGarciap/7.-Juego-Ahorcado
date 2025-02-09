6856642f5283da4d530900467ae795df97a0bf06
NachoGarcia <ignaciogarcia.dev@gmail.com>
2025-02-09 19:37:06 +0100

    mejor estructuracion del juego

--- Juego ahorcado/juego.py
+++ Juego ahorcado/juego.py
@@ -1,44 +1,93 @@
 import random
 
-contador_intentos = 6
 
-palabras = ['h', 'pe', 'cuc', 'pito']
-letras_usadas = []  # Para guardar las letras que ya se han usado
+class juegoAhorcado:
 
-palabra_aleatoria = random.choice(palabras)
+    def __init__(self):
+        self.contador_intentos = 6
+        self.palabras = ['h', 'pe', 'cuc', 'pito']
+        self.palabras_animales = ['leon', 'hipopotamo', 'tigre', 'mono', 'perro']
+        self.palabras_muebles = ['silla', 'mesa', 'estanteria', 'cama', 'lampara']
+        self.palabras_colores = ['negro', 'blanco', 'azul', 'amarillo', 'verde']
+        self.letras_usadas = []  # Para guardar las letras que ya se han usado
+        self.palabra_oculta = []
+        self.palabra_aleatoria = ''
 
-palabra_oculta = ['_'] * len(palabra_aleatoria)  # Representar la palabra oculta con guiones bajos
 
-print("----- Bienvenido al juego del Ahorcado! -----")
+        print("----- Bienvenido al juego del Ahorcado! -----")
 
-while contador_intentos > 0 and '_' in palabra_oculta:
-    print("Palabra a adivinar: " + " ".join(palabra_oculta))  # unimos los elementos de la lista
-    print(f"Letras ya usadas: {', '.join(letras_usadas)}")
+    def menu_juego(self):
+        while True:
+            print('Categorias:')
+            print('----------------------')
+            print('|1. Animales(nivel 1)|')
+            print('|2. Muebles (nivel 2)|')
+            print('|3. Colores (nivel 3)|')
+            print('----------------------')
 
-    letra_usuario = input('Introduce una letra: ')
-    if letra_usuario in letras_usadas:
-        print("Ya has usado esa letra. Intenta con otra.")
-        continue  # Vuelve al inicio del bucle si ya se usó la letra
+            try:
+                opcion = int(input("Elige un nivel para empezar a jugar: "))
+            except ValueError:
+                print("Por favor, introduce un número válido.")
+                continue
 
-    letras_usadas.append(letra_usuario)  # Añadir la letra a la lista de letras usadas
+            if opcion == 1:
+                self.seleccionar_categoria(self.palabras_animales)
+                break
+            elif opcion == 2:
+                self.seleccionar_categoria(self.palabras_muebles)
+                break
+            elif opcion == 3:
+                self.seleccionar_categoria(self.palabras_colores)
+                break
+            elif opcion == 4:
+                print('Saliendo del juego...')
+                break
+            else:
+                print('Introduce una categoria valida')
 
-    if letra_usuario in palabra_aleatoria:
-        print('la letra esta en la palabra')
-        for i in range(len(palabra_aleatoria)):
-            if palabra_aleatoria[i] == letra_usuario:
-                palabra_oculta[i] = letra_usuario
+    def seleccionar_categoria(self, lista_palabras):
+        self.palabra_aleatoria = random.choice(lista_palabras)
+        self.palabra_oculta = ['_'] * len(self.palabra_aleatoria)  # Representar la palabra oculta con guiones bajos
+        self.ejecutar_juego()
 
-        print(" ".join(palabra_oculta))
+    def ejecutar_juego(self):
+        while self.contador_intentos > 0 and '_' in self.palabra_oculta:
+            print('--------------------')
+            print("Palabra a adivinar: " + " ".join(self.palabra_oculta))  # unimos los elementos de la lista
+            print(f"Letras ya usadas: {', '.join(self.letras_usadas)}")
 
-    else:
-        contador_intentos -= 1
-        print('Letra incorrecta')
-        print(f'Te quedan {contador_intentos} intentos!')
+            letra_usuario = input('Introduce una letra: ')
 
-    if '_' not in palabra_oculta:
-        print(f'Has ganado!La palabra aleatoria es: {palabra_aleatoria}')
+            if len(letra_usuario) != 1:
+                print('Por favor, introduce solo una letra!')
+                continue  # Si el usuario ingresa más de una letra, vuelve al inicio
 
-else:
-    if contador_intentos == 0:
-        print(f'Has perdido!, la palabra aleatoria era: {palabra_aleatoria}')
+            if letra_usuario in self.letras_usadas:
+                print("Ya has usado esa letra. Intenta con otra.")
+                continue  # Vuelve al inicio del bucle si ya se usó la letra
+
+            self.letras_usadas.append(letra_usuario)  # Añadir la letra a la lista de letras usadas
+
+            if letra_usuario in self.palabra_aleatoria:
+                print('la letra esta en la palabra')
+                for i in range(len(self.palabra_aleatoria)):
+                    if self.palabra_aleatoria[i] == letra_usuario:
+                        self.palabra_oculta[i] = letra_usuario
+
+                print(" ".join(self.palabra_oculta))
+
+            else:
+                self.contador_intentos -= 1
+                print('Letra incorrecta')
+                print(f'Te quedan {self.contador_intentos} intentos!')
+
+            if '_' not in self.palabra_oculta:
+                print(f'Has ganado!La palabra aleatoria es: {self.palabra_aleatoria}')
+
+        else:
+            if self.contador_intentos == 0:
+                print(f'Has perdido!, la palabra aleatoria era: {self.palabra_aleatoria}')
+
+
 
---
+++ Juego ahorcado/main.py
@@ -0,0 +1,4 @@
+from juego import juegoAhorcado
+
+jugar = juegoAhorcado()
+jugar.menu_juego()
